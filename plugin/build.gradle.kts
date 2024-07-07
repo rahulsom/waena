8d1e8894552eccc508f10b530cb0792bbe3d17a6
Rahul Somasunderam <rahul.som@gmail.com>
2024-07-07 15:25:47 -0700

    chore: Add plugin tags for gradle

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -27,12 +27,14 @@ gradlePlugin {
     implementationClass = "com.github.rahulsom.waena.WaenaRootPlugin"
     displayName = "Waena Plugin for Root Modules"
     description = "Configures the root project to publish to Maven Central."
+    tags.set(mutableSetOf("publishing", "mavencentral"))
   }
   val waenaPublished by plugins.creating {
     id = "com.github.rahulsom.waena.published"
     implementationClass = "com.github.rahulsom.waena.WaenaPublishedPlugin"
     displayName = "Waena Plugin for Published Modules"
     description = "Marks a module as one to be published to Maven Central."
+    tags.set(mutableSetOf("publishing", "mavencentral"))
   }
   website.set("https://github.com/rahulsom/waena")
   vcsUrl.set("https://github.com/rahulsom/waena.git")
