3b61dcb1f74d472234cb127a99adce261cfed0bb
Rahul Somasunderam <rahul.som@gmail.com>
2025-09-03 22:54:23 -0700

    refactor: Use parameterized test
    This allows testing against multiple Gradle versions.

--- waena-plugin/src/test/kotlin/com/github/rahulsom/waena/WaenaPluginFunctionalTest.kt
+++ waena-plugin/src/test/kotlin/com/github/rahulsom/waena/WaenaPluginFunctionalTest.kt
@@ -1,28 +1,20 @@
 package com.github.rahulsom.waena
 
+import com.github.rahulsom.waena.WaenaRootPlugin.Companion.CENTRAL
+import com.github.rahulsom.waena.WaenaRootPlugin.Companion.OSS
+import com.github.rahulsom.waena.WaenaRootPlugin.Companion.S01
 import org.assertj.core.api.SoftAssertions
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.transport.URIish
 import org.gradle.testkit.runner.GradleRunner
 import org.intellij.lang.annotations.Language
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 import java.io.File
-
-private const val SNAPSHOT = "snapshot"
-private const val FINAL = "final"
-
-private const val PUBLISH_SONATYPE = "publishNebulaPublicationToSonatypeRepository"
-private const val CLOSE_SONATYPE = "closeSonatypeStagingRepository"
-private const val PUBLISH_LOCAL = "publishNebulaPublicationToLocalRepository"
-private const val RELEASE_SONATYPE = "releaseSonatypeStagingRepository"
-
-private val ALL_TASKS = setOf(
-  PUBLISH_LOCAL,
-  PUBLISH_SONATYPE,
-  CLOSE_SONATYPE,
-  RELEASE_SONATYPE
-)
 
 /**
  * A simple functional test for the 'com.github.rahulsom.waena.greeting' plugin.
@@ -30,6 +22,17 @@ private val ALL_TASKS = setOf(
 class WaenaPluginFunctionalTest {
 
   companion object {
+    private const val SNAPSHOT = "snapshot"
+    private const val FINAL = "final"
+
+    private const val SONATYPE_1_PUBLISH = "publishNebulaPublicationToSonatypeRepository"
+    private const val SONATYPE_2_CLOSE = "closeSonatypeStagingRepository"
+    private const val LOCAL_PUBLISH = "publishNebulaPublicationToLocalRepository"
+    private const val SONATYPE_3_RELEASE = "releaseSonatypeStagingRepository"
+
+    private val ALL_TASKS = setOf(LOCAL_PUBLISH, SONATYPE_1_PUBLISH, SONATYPE_2_CLOSE, SONATYPE_3_RELEASE)
+    private val PUBLISH_TASKS = setOf(LOCAL_PUBLISH, SONATYPE_1_PUBLISH)
+
     fun buildGradleScript(waenaConfig: String = ""): String {
       // language=groovy
       return """
@@ -52,94 +55,42 @@ class WaenaPluginFunctionalTest {
             }
         """
     }
+
+    @JvmStatic
+    fun testParameters(): Stream<Arguments> {
+      val gradleVersion = listOf(null, "9.0.0")
+      val configUrlPairs = listOf(
+        Pair(null, CENTRAL),
+        Pair("OSS", OSS),
+        Pair("Central", CENTRAL),
+        Pair("S01", S01)
+      )
+      val taskTasksPairs = listOf(
+        Pair(SNAPSHOT, PUBLISH_TASKS),
+        Pair(FINAL, ALL_TASKS)
+      )
+      return gradleVersion.flatMap { gv ->
+        configUrlPairs.flatMap { (publishMode, urls) ->
+          taskTasksPairs.map { (task, tasks) ->
+            arguments(publishMode, task, tasks, urls, gv)
+          }
+        }
+      }.stream()
+    }
   }
 
-  @Test
-  fun `snapshot without customization`(@TempDir projectDir: File) {
-    runTest(
-      "",
-      SNAPSHOT,
-      setOf(PUBLISH_SONATYPE, PUBLISH_LOCAL),
-      WaenaRootPlugin.CENTRAL,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `final without customization`(@TempDir projectDir: File) {
-    runTest(
-      "",
-      FINAL,
-      setOf(PUBLISH_LOCAL, PUBLISH_SONATYPE, CLOSE_SONATYPE, RELEASE_SONATYPE),
-      WaenaRootPlugin.CENTRAL,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `snapshot for OSS`(@TempDir projectDir: File) {
-    runTest(
-      "waena { publishMode.set(PublishMode.OSS) } ",
-      SNAPSHOT,
-      setOf(PUBLISH_LOCAL, PUBLISH_SONATYPE),
-      WaenaRootPlugin.OSS,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `final for OSS`(@TempDir projectDir: File) {
-    runTest(
-      "waena { publishMode.set(PublishMode.OSS) } ",
-      FINAL,
-      setOf(PUBLISH_LOCAL, PUBLISH_SONATYPE, CLOSE_SONATYPE, RELEASE_SONATYPE),
-      WaenaRootPlugin.OSS,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `snapshot for Central`(@TempDir projectDir: File) {
-    runTest(
-      "waena { publishMode.set(PublishMode.Central) } ",
-      SNAPSHOT,
-      setOf(PUBLISH_LOCAL, PUBLISH_SONATYPE),
-      WaenaRootPlugin.CENTRAL,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `final for Central`(@TempDir projectDir: File) {
-    runTest(
-      "waena { publishMode.set(PublishMode.Central) } ",
-      FINAL,
-      setOf(PUBLISH_LOCAL, RELEASE_SONATYPE, CLOSE_SONATYPE, PUBLISH_SONATYPE),
-      WaenaRootPlugin.CENTRAL,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `snapshot for S01`(@TempDir projectDir: File) {
-    runTest(
-      "waena { publishMode.set(PublishMode.S01) } ",
-      SNAPSHOT,
-      setOf(PUBLISH_LOCAL, PUBLISH_SONATYPE),
-      WaenaRootPlugin.S01,
-      projectDir
-    )
-  }
-
-  @Test
-  fun `final for S01`(@TempDir projectDir: File) {
-    runTest(
-      "waena { publishMode.set(PublishMode.S01) } ",
-      FINAL,
-      setOf(PUBLISH_LOCAL, PUBLISH_SONATYPE, CLOSE_SONATYPE, RELEASE_SONATYPE),
-      WaenaRootPlugin.S01,
-      projectDir
-    )
+  @ParameterizedTest(name = "gradle({4}) publishMode({0}) task({1})")
+  @MethodSource("testParameters")
+  fun testWaenaConfiguration(
+    testName: String?,
+    task: String,
+    containsTasks: Set<String>,
+    urls: Pair<String, String>,
+    gradleVersion: String?,
+    @TempDir projectDir: File
+  ) {
+    val waenaConfig = if (testName == null) "" else "waena { publishMode.set(PublishMode.$testName) }"
+    runTest(waenaConfig, task, containsTasks, urls, gradleVersion, projectDir)
   }
 
   fun runTest(
@@ -147,6 +98,7 @@ class WaenaPluginFunctionalTest {
     task: String,
     containsTasks: Set<String>,
     urls: Pair<String, String>,
+    gradleVersion: String?,
     @TempDir projectDir: File
   ) {
     val doesNotContainTasks = ALL_TASKS - containsTasks
@@ -168,8 +120,8 @@ class WaenaPluginFunctionalTest {
     git.commit().setMessage("Initial commit").call()
     git.remoteAdd().setName("origin").setUri(URIish("https://github.com/rahulsom/nothing.git")).call()
 
-    val taskTree = getTaskTree(task, projectDir)
-    val showConfigResult = showConfig(projectDir)
+    val taskTree = getTaskTree(task, projectDir, gradleVersion)
+    val showConfigResult = showConfig(projectDir, gradleVersion)
 
     println(showConfigResult + "\n" + taskTree)
 
@@ -187,17 +139,23 @@ class WaenaPluginFunctionalTest {
     softly.assertAll()
   }
 
-  private fun getTaskTree(task: String, projectDir: File): String? {
-    val runner = baseRunner(projectDir)
+  private fun getTaskTree(task: String, projectDir: File, gradleVersion: String?): String? {
+    val runner = baseRunner(projectDir, gradleVersion)
     runner.withArguments(task, "taskTree")
     val result = runner.build().output
     return result
   }
 
-  private fun baseRunner(projectDir: File): GradleRunner = GradleRunner.create().forwardOutput().withPluginClasspath().withProjectDir(projectDir)
+  private fun baseRunner(projectDir: File, gradleVersion: String?): GradleRunner {
+    val runner = GradleRunner.create().forwardOutput().withPluginClasspath().withProjectDir(projectDir)
+    if (gradleVersion != null) {
+      runner.withGradleVersion(gradleVersion)
+    }
+    return runner
+  }
 
-  private fun showConfig(projectDir: File): String? {
-    val showConfigRunner = baseRunner(projectDir)
+  private fun showConfig(projectDir: File, gradleVersion: String?): String? {
+    val showConfigRunner = baseRunner(projectDir, gradleVersion)
     showConfigRunner.withArguments("showconfig")
     val showConfigResult = showConfigRunner.build().output
     return showConfigResult
